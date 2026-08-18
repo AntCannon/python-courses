a441ba4c18cc4fad7b69a269469987fd473d9da0
AntCannon <anthonycannonier@gmail.com>
2026-08-18 17:43:56 -0400

    add  hello world and hello pthon views

--- django_full_crash_course/tutorialproject/todos/views.py
+++ django_full_crash_course/tutorialproject/todos/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+"""
+def function(http request):
+    return http response
+
+"""
+
+
+#  mysite.com/hello -> Hello World
+def hello_world_view(request):
+    return HttpResponse("Hello World")
+
+
+def hello_python_view(request):
+    return HttpResponse("Hello Python")
