4cd50d952e70e73f44e37069ffc74b1fb79199b9
AntCannon <anthonycannonier@gmail.com>
2026-08-18 12:34:07 -0400

    add mocker comments

--- pyTest/test_main_06.py
+++ pyTest/test_main_06.py
@@ -2,15 +2,21 @@ import pytest
 from main_06 import get_weather
 
 
+# call mocker
 def test_get_weather(mocker):
     # Mock requests.get
     mock_get = mocker.patch("main_06.requests.get")
 
     # Set return values
+    # from the response return the status code value
     mock_get.return_value.status_code = 200
+
+    # from the response return the json value
+    # from json return value
+    # json is a function so has access to the return_value
     mock_get.return_value.json.return_value = {"temperature": 25, "condition": "Sunny"}
 
-    # Call function
+    # Call function using hte mock config
     result = get_weather("Dubai")
 
     # Assertions
