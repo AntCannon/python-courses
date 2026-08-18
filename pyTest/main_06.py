34ffa43cd524a104f60a3a7a80350a21a1d65d54
AntCannon <anthonycannonier@gmail.com>
2026-08-18 14:49:17 -0400

    format

--- pyTest/main_06.py
+++ pyTest/main_06.py
@@ -2,7 +2,9 @@ import requests
 
 
 def get_weather(city):
-    response = requests.get(f"https://api.weather.com/v1/{city}")
+    response = requests.get(
+        f"https://api.weather.com/v1/{city}"
+    )  # we want to mock the api to test code locally
     if response.status_code == 200:
         return response.json()
     else:
