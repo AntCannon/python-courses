ff280ed4c289b740e8f670864729e390b83405ec
AntCannon <anthonycannonier@gmail.com>
2026-08-19 09:17:01 -0400

    html render and query

--- django_full_crash_course/tutorialproject/todos/urls.py
+++ django_full_crash_course/tutorialproject/todos/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path("hello", views.hello_world_view, name="hello_world"),
     path("", views.hello_python_view, name="hello_python"),
+    path("htmlrender", views.hello_html_view, name="hello_html"),
+    path("helloname/<str:name>", views.hello_path_view, name="hello_name"),
+    path("add/<int:num1>/<int:num2>", views.add_view, name="add"),
 ]
--- django_full_crash_course/tutorialproject/todos/views.py
+++ django_full_crash_course/tutorialproject/todos/views.py
@@ -11,9 +11,27 @@ def function(http request):
 
 
 #  mysite.com/hello -> Hello World
+# set url path in urls.py urlpatterns list
 def hello_world_view(request):
     return HttpResponse("Hello World")
 
 
 def hello_python_view(request):
     return HttpResponse("Hello Python")
+
+
+# Render HTML page
+def hello_html_view(request):
+    # return render(request, <html file path>)
+    # html file has to be in path ./templates/<app name>
+    return render(request, "todos/hello.html")
+
+
+# you get parameters from a url from urlpattern
+# path("helloname/<str:name>", views.hello_path_view, name="hello_name"),
+def hello_path_view(request, name):
+    return HttpResponse(f"Hello {name}!")
+
+
+def add_view(request, num1, num2):
+    return HttpResponse(f"Sum is {num1 + num2}")
